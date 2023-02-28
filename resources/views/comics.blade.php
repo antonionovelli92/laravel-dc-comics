41f3ce80acb0d6a439d94a99a4bacf92dd116f1e
antonionovelli92 <anto.novelli@hotmail.it>
2023-02-28 19:10:57 +0100

    add migrate in phpmyadmin

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -4,4 +4,14 @@
 
 @section('content')
     <h1 class="text-center p-5">comics</h1>
+    <div class="deck d-flex justify-content-center align-items-center flex-wrap">
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                    content.</p>
+            </div>
+        </div>
+    </div>
+
 @endsection
