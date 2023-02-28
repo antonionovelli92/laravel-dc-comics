202980fadbbb23787b6cd834fb8115c4809c2865
antonionovelli92 <anto.novelli@hotmail.it>
2023-02-28 19:11:16 +0100

    migrate

--- database/migrations/2023_02_28_175127_create_comics_table.php
+++ database/migrations/2023_02_28_175127_create_comics_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('thumb')->nullable();
+            $table->decimal('price', 5, 2)->nullable();
+            $table->string('series');
+            $table->date('sale_date')->nullable();
+            $table->string('type');
+            $table->text('artists')->nullable();
+            $table->text('writers')->nullable();
             $table->timestamps();
         });
     }
